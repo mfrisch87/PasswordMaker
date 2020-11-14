38b0f3829c6353dd1d60075f7f73435423178481
mfrisch87 <mikey.frisch@gmail.com>
2020-11-13 16:59:34 -0800

    Added letter and number range. Also button listener

---
+++ script.js
@@ -0,0 +1,45 @@
+// Character choices
+
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVWYX' + 'abcdefghijklmnopqrctuvwyz' ; 
+    numbers = '1234567890';
+    characters = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Button Listener
+document.getElementById("redbutton").addEventListener("click", function() {
+    prompt("How many characters would you like your password to be (between 8 to 40 characters");
+    alert("Upper case letters?");
+    alert("Lower case letters?");
+    alert("Special Characters?");
+})
