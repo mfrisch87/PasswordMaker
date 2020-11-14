418119b3024aa592cd7ae73ace0029edb0e6dc73
mfrisch87 <mikey.frisch@gmail.com>
2020-11-13 17:44:27 -0800

    fixed confirms

--- script.js
+++ script.js
@@ -40,15 +40,15 @@ var letters = 'ABCDEFGHIJKLMNOPQRSTUVWYX' + 'abcdefghijklmnopqrctuvwyz' ;
 document.getElementById("redbutton").addEventListener("click", function() {
     // Prompt User for parameters for password
     var length = prompt("How many characters would you like your password to be (between 8 to 40 characters");
-    var upper = alert("Upper case letters?");
-    var lower = alert("Lower case letters?");
-    var special = alert("Special Characters?");
+    var upper = confirm("Upper case letters?");
+    var lower = confirm("Lower case letters?");
+    var special = confirm("Special Characters?");
 })
 
 // Generate new password
 function newPassword() {
     var password = '';
-    
+
 }
 
 // Apprend new password to textarea
